736288cbdf893b22dad2e0ff328546bf47a30903
Darshan-V <mr.darshanv@gmail.com>
2022-07-26 12:34:21 +0530

    ----

--- script.js
+++ script.js
@@ -3,34 +3,35 @@ const todoForm = document.querySelector('.todo-form')
 const todoInput = document.querySelector('.todo-input')
 const todoItemsList = document.querySelector('.todo-items')
 let todos = []
-let notes = []
 
-// const priorities = ['High', 'Medium', 'Low', 'None']
+const priorities = { none: 0, low: 1, medium: 2, high: 3 }
 
 todoForm.addEventListener('submit', function (event) {
   event.preventDefault()
   addTodo(todoInput.value)
 })
 
-// function to add todo
+
+
 function addTodo(item) {
-  // if item is not empty
   if (item === '') {
     return
   }
   const todo = {
-    note: "",
+    noteName: '',
     id: Date.now(),
     name: item,
-    completed: false
+    completed: false,
+    priority: priorities.none,
+    dueDate: ''
   }
-  // then add it to todos array
   todos.push(todo)
   addToLocalStorage(todos)
   todoInput.value = ''
 }
 
-function renderTodos(todos) {
+
+function renderMain(todos) {
   // console.log(todos)
   todoItemsList.innerHTML = ''
   todos.forEach(function (item) {
@@ -43,57 +44,77 @@ function renderTodos(todos) {
       li.classList.add('checked')
     }
     li.innerHTML = `
-      <input type="checkbox" class="checkbox" ${checked}>
-      ${item.name}
-      <button class='show' onclick = 'showDiv()'>▼</button>
+      <input type = 'checkbox' class = 'checkbox' ${checked}>
+      
+      <button class = 'show' onclick = 'showDiv()'>▼</button>
 
     `
-    todoItemsList.append(li)
+    const listDiv = document.createElement('div')
+    listDiv.innerText = `${item.name}`
+    li.append(listDiv)
 
+    todoItemsList.append(li)
+  })
+}
+
+function renderHidden() {
+
+  todos.forEach(function (item) {
+
+    const li = document.querySelector('li')
     const listContainer = document.createElement('div')
     listContainer.setAttribute('class', 'listContainer')
     listContainer.setAttribute('data-key', item.id)
 
-    listContainer.innerHTML = `<label for="due-date"></label>
-    <input type="date" id="Duedate" name="Duedate"/>
-    <form>
-    <textarea type='text' class= "note">${item.note}</textarea>
-    </form>
-    <select class = 'priorities' name="Priority">
-    <option value="none">None</opiton>
-    <option value="greem">Medium</option>
-    <option value ="red">High</option>
+    listContainer.innerHTML = `
+    <input type = 'date' id = 'Duedate' name = 'Duedate'/>
+
+    <div class= 'notes-content'>
+    <form class = 'note-form'>
+      <textarea class= 'list-item__textarea'>${item.noteName}</textarea>
+      </form>
+    </div>
+    
+    <select class = 'priorities' name= 'Priority'>
+      <option value = 'none'>None</opiton>
+      <option value = 'low'>Low</option>
+      <option value = 'medium'>Medium</option>
+      <option value = 'high'>High</option>
     </select>
-    <button class="delete-button">Delete</button>
+    
+    <button class ='delete-button'>Delete</button>
     `
     li.appendChild(listContainer)
     listContainer.style.display = 'none'
   })
 }
 
-
 // function to add todos to local storage
-function addToLocalStorage(todos, notes) {
+function addToLocalStorage(todos) {
   localStorage.setItem('todos', JSON.stringify(todos))
-  // localStorage.setItem('notes', JSON.stringify(notes))
-  renderTodos(todos)
-
-  // localStorage.setItem('notes', JSON.stringify(notes))
-  // renderNotes(notes)
+  renderMain(todos)
+  renderHidden(todos)
 }
 
 function getFromLocalStorage() {
   const reference = localStorage.getItem('todos')
   if (reference) {
     todos = JSON.parse(reference)
-    renderTodos(todos)
+    renderMain(todos)
+    renderHidden(todos)
   }
-  // const noteReference = localStorage.getItem('notes')
-  // if (noteReference) {
-  // notes = JSON.parse(noteReference)
-  // renderNotes(notes)
-  // }
 }
+
+function showDiv() {
+  const x = document.querySelector('.listContainer')
+
+  if (x.style.display === 'none') {
+    x.style.display = 'block'
+  } else {
+    x.style.display = 'none'
+  }
+}
+
 
 function toggle(id) {
   todos.forEach(function (item) {
@@ -109,6 +130,10 @@ function deleteTodo(id) {
   })
 }
 
+function setDate() {
+
+}
+
 getFromLocalStorage()
 todoItemsList.addEventListener('click', function (event) {
   // check if the event is on checkbox
@@ -121,62 +146,3 @@ todoItemsList.addEventListener('click', function (event) {
     addToLocalStorage(todos)
   }
 })
-
-// todoItemsList.addEventListener('change', function (event) {
-//   if (event.target.classList.contains('note')) {
-//     addToLocalStorage(notes)
-//   }
-// })
-
-function showDiv() {
-  const x = document.querySelector('.listContainer')
-  if (x.style.display === 'none') {
-    x.style.display = 'block'
-  } else {
-    x.style.display = 'none'
-  }
-}
-
-const noteData = document.querySelector('.note')
-console.log(noteData)
-
-function updateNote(text) {
-  if (text = '') {
-    return
-  }
-  const note = {
-    note: text
-  }
-  notes.push(note)
-  addToLocalStorage(notes)
-  noteData = ''
-}
-
-// noteData.addEventListener('change', function (event) {
-//   updateNote(todoInput.value)
-// })
-
-// function renderNotes(notes) {
-
-//   notes.forEach(function (item) {
-
-
-//     const listContainer = document.createElement('div')
-//     listContainer.setAttribute('class', 'listContainer')
-//     listContainer.setAttribute('data-key', item.id)
-//     listContainer.innerHTML = `<label for="due-date"></label>
-//   <input type="date" id="Duedate" name="Duedate"/>
-//   <form>
-//   <textarea type='text' class= "note">${notes}</textarea>
-//   </form>
-//   <select class = 'priorities' name="Priority">
-//   <option value="none">None</opiton>
-//   <option value="greem">Medium</option>
-//   <option value ="red">High</option>
-//   </select>
-//   <button class="delete-button">Delete</button>
-//   `
-//     li.appendChild(listContainer)
-//     listContainer.style.display = 'none'
-//   })
-// }
